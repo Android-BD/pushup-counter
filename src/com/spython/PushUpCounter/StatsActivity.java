ca3a59c3baa9e6a03ddfcbbd23570353146b0ee0
SPython <rvan.mega@gmail.com>
2014-07-13 20:40:34 +0600

    Add chart

--- src/com/spython/PushUpCounter/StatsActivity.java
+++ src/com/spython/PushUpCounter/StatsActivity.java
@@ -2,10 +2,16 @@ package com.spython.PushUpCounter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
 import java.util.ArrayList;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.BarGraphView;
+import com.jjoe64.graphview.GraphViewSeries;
 
 public class StatsActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -16,12 +22,14 @@ public class StatsActivity extends Activity {
 		setContentView(R.layout.stats);
 		PushUpData pushUpData = new PushUpData(getApplicationContext());
 		
+		LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
 		TextView total = (TextView) findViewById(R.id.total);
 		TextView average = (TextView) findViewById(R.id.average);
 		TextView speed = (TextView) findViewById(R.id.speed);
 		
 		List data = new ArrayList();
 		data = pushUpData.getData();
+		GraphViewData[] gData = new GraphViewData[data.size()];
 		
 		int totalCount = 0;
 		long averageSpeed = 0;
@@ -32,7 +40,13 @@ public class StatsActivity extends Activity {
 			int count = (Integer) temp.get(0);
 			totalCount += count;
 			averageSpeed += (Long) temp.get(2) / count;
+			int[] tmp = (int[]) temp.get(1);
+			gData[i] = new GraphViewData(tmp[2], (double) count);
 		}
+		
+		GraphViewSeries series = new GraphViewSeries(gData);
+		GraphView graphView = new BarGraphView(this, "Push ups");
+		graphView.addSeries(series);
 		
 		if (i == 0) {
 			i = 1;
@@ -50,5 +64,7 @@ public class StatsActivity extends Activity {
 			String.valueOf(averageCount));
 		speed.setText("Average time needed to make a push up: " +
 			String.valueOf(averageSpeedSec) + measure);
+		
+		layout.addView(graphView);
 	}
 }
